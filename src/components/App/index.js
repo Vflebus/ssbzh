73edee0c0358239c453067b10ace553684f69c18
Vincent Flebus <vflebus@gmail.com>
2022-05-10 16:57:53 +0200

    stroke sur les vagues

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -155,6 +155,18 @@ function App() {
         )}
         <img src={logo} className="logoBreizh" alt="" />
       </div>
+      <svg className="waves" ref={waves} xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
+        viewBox="0 24 150 28" preserveAspectRatio="none" shapeRendering="auto">
+        <defs>
+          <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+        </defs>
+        <g className="parallax">
+          <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(31, 64, 73, 0.7" stroke="rgba(255, 255, 255, 0.7" stroke-width=".4" />
+          <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(31, 64, 73, 0.5)" stroke="rgba(255, 255, 255, 0.5" stroke-width=".4" />
+          <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(31, 64, 73, 0.3)" stroke="rgba(255, 255, 255, 0.3" stroke-width=".4" />
+          <use xlinkHref="#gentle-wave" x="48" y="9" fill="#50a4b9" stroke='white' stroke-width=".4" />
+        </g>
+      </svg>
       <div className="sea" ref={sea}>
         <div className="seaNav">
           <button className='diveButton' onClick={diveOut} ref={diveOutButton}>Retour aux tournois</button>
@@ -192,18 +204,6 @@ function App() {
           <Player name={tournament[selectedPlayer].nom} seed={tournament[selectedPlayer].seed} matchs={tournament[selectedPlayer].matchs} finalRank={tournament[selectedPlayer].classement_final} />
         )}
       </div>
-      <svg className="waves" ref={waves} xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
-        viewBox="0 24 150 28" preserveAspectRatio="none" shapeRendering="auto">
-        <defs>
-          <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
-        </defs>
-        <g className="parallax">
-          <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(31, 64, 73, 0.7" />
-          <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(31, 64, 73, 0.5)" />
-          <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(31, 64, 73, 0.3)" />
-          <use xlinkHref="#gentle-wave" x="48" y="9" fill="#50a4b9" />
-        </g>
-      </svg>
     </div>
   );
 }
